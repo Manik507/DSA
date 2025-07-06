5365b91fcba33d3bf510a5ab9ee94651a8cc6d81
MANIK BARAD <161632764+Manik507@users.noreply.github.com>
2025-07-07 00:16:46 +0530

    Add files via upload

--- top_k_sum_pairs.cpp
+++ top_k_sum_pairs.cpp
@@ -1,20 +1,28 @@
-
 #include <bits/stdc++.h>
 using namespace std;
 
 class Solution {
   public:
     vector<int> topKSumPairs(vector<int>& a, vector<int>& b, int k) {
+        // Sort both arrays in descending order
         sort(a.rbegin(), a.rend());
         sort(b.rbegin(), b.rend());
+
+        // Max-heap to store sum and their indices
         priority_queue<pair<int, pair<int, int>>> pq;
+
+        // Set to track which index pairs we have already used
         set<pair<int, int>> vis;
+
+        // Vector to store final answers
         vector<int> ans;
         int n = a.size();
 
+        // Push the first largest sum (first elements of both arrays)
         pq.push({a[0] + b[0], {0, 0}});
         vis.insert({0, 0});
 
+        // Keep adding top K largest sums
         while (ans.size() < k) {
             auto top = pq.top();
             pq.pop();
@@ -23,12 +31,15 @@ class Solution {
             int i = top.second.first;
             int j = top.second.second;
 
-            ans.push_back(sum);
+            ans.push_back(sum);  // store current largest sum
 
+            // If possible, add next pair (i+1, j)
             if (i < n-1 && vis.find({i+1, j}) == vis.end()) {
                 pq.push({a[i+1] + b[j], {i+1, j}});
                 vis.insert({i+1, j});
             }
+
+            // If possible, add next pair (i, j+1)
             if (j < n-1 && vis.find({i, j+1}) == vis.end()) {
                 pq.push({a[i] + b[j+1], {i, j+1}});
                 vis.insert({i, j+1});
@@ -39,6 +50,7 @@ class Solution {
 };
 
 int main() {
+    // Example input arrays
     vector<int> a = {4, 2, 1};
     vector<int> b = {5, 3, 1};
     int k = 4;
@@ -46,6 +58,7 @@ int main() {
     Solution obj;
     vector<int> result = obj.topKSumPairs(a, b, k);
 
+    // Print the top K sum pairs
     cout << "Top " << k << " sum pairs: ";
     for (int num : result)
         cout << num << " ";
